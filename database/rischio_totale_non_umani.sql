f8be521cf78af53843c430d304ff6bfec93d0d76
Mauro Bartolomeoli <maurobartolomeoli@gmail.com>
2013-03-04 18:18:18 +0100

    Updated risk formula queries

--- database/rischio_totale_non_umani.sql
+++ database/rischio_totale_non_umani.sql
@@ -1,122 +1,198 @@
 select siig_geo_ln_arco_1.id_geo_arco,siig_geo_ln_arco_1.nr_incidenti_elab * (
      select sum(siig_r_arco_1_sostanza.padr * 
      (
-     select sum(siig_r_scenario_sostanza.psc * (
-        (select mq_aree_agricole
+     select sum(siig_r_scenario_sostanza.psc * ((
+        (select coalesce(mq_aree_agricole,1)
         from siig_t_vulnerabilita_1
-        where id_geo_arco = siig_geo_ln_arco_1.id_geo_arco) * (1 - (
-        select cff 
+        where id_geo_arco = siig_geo_ln_arco_1.id_geo_arco
+        and id_distanza = (
+             select fk_distanza
+             from siig_r_area_danno
+             inner join siig_d_gravita on siig_r_area_danno.id_gravita = siig_d_gravita.id_gravita
+             where siig_d_gravita.fk_bersaglio = 13             
+             and siig_r_area_danno.id_scenario = siig_r_scenario_sostanza.id_scenario
+             and siig_r_area_danno.id_sostanza = siig_r_scenario_sostanza.id_sostanza
+             and siig_r_area_danno.flg_lieve = siig_r_scenario_sostanza.flg_lieve
+        )
+        ) * (1 - coalesce((
+        select cff
         from siig_r_arco_1_scen_tipobers
         where id_geo_arco = siig_geo_ln_arco_1.id_geo_arco
-        and id_scenario = 1--siig_r_scenario_sostanza.id_scenario
-        and id_bersaglio = 3--13
-        )) * (
+        and id_scenario = siig_r_scenario_sostanza.id_scenario
+        and id_bersaglio = 13
+        ),0.3)) * (
                 select suscettibilita
                 from siig_r_scenario_gravita
                 inner join siig_d_gravita on siig_r_scenario_gravita.id_gravita = siig_d_gravita.id_gravita
-                where id_scenario = 1--siig_r_scenario_sostanza.id_scenario
+                where id_scenario = siig_r_scenario_sostanza.id_scenario
                 and fk_bersaglio = 13
-        ) + 
-        (select mq_aree_boscate
+        ) * 1
+     ) + (
+        (select coalesce(mq_aree_protette,1)
         from siig_t_vulnerabilita_1
-        where id_geo_arco = siig_geo_ln_arco_1.id_geo_arco) * (1 - (
-        select cff 
+        where id_geo_arco = siig_geo_ln_arco_1.id_geo_arco
+        and id_distanza = (
+             select fk_distanza
+             from siig_r_area_danno
+             inner join siig_d_gravita on siig_r_area_danno.id_gravita = siig_d_gravita.id_gravita
+             where siig_d_gravita.fk_bersaglio = 12             
+             and siig_r_area_danno.id_scenario = siig_r_scenario_sostanza.id_scenario
+             and siig_r_area_danno.id_sostanza = siig_r_scenario_sostanza.id_sostanza
+             and siig_r_area_danno.flg_lieve = siig_r_scenario_sostanza.flg_lieve
+        )) * (1 - coalesce((
+        select cff
         from siig_r_arco_1_scen_tipobers
         where id_geo_arco = siig_geo_ln_arco_1.id_geo_arco
-        and id_scenario = 1--siig_r_scenario_sostanza.id_scenario
-        and id_bersaglio = 3--11
-        )) * (
+        and id_scenario = siig_r_scenario_sostanza.id_scenario
+        and id_bersaglio = 12
+        ),0.3)) * (
                 select suscettibilita
                 from siig_r_scenario_gravita
                 inner join siig_d_gravita on siig_r_scenario_gravita.id_gravita = siig_d_gravita.id_gravita
-                where id_scenario = 1--siig_r_scenario_sostanza.id_scenario
-                and fk_bersaglio = 13--11
-        ) + 
-        (select mq_aree_protette
+                where id_scenario = siig_r_scenario_sostanza.id_scenario
+                and fk_bersaglio = 12
+        ) * 1
+     ) + (
+        (select coalesce(mq_aree_boscate,1)
         from siig_t_vulnerabilita_1
-        where id_geo_arco = siig_geo_ln_arco_1.id_geo_arco) * (1 - (
-        select cff 
+        where id_geo_arco = siig_geo_ln_arco_1.id_geo_arco
+        and id_distanza = (
+             select fk_distanza
+             from siig_r_area_danno
+             inner join siig_d_gravita on siig_r_area_danno.id_gravita = siig_d_gravita.id_gravita
+             where siig_d_gravita.fk_bersaglio = 11             
+             and siig_r_area_danno.id_scenario = siig_r_scenario_sostanza.id_scenario
+             and siig_r_area_danno.id_sostanza = siig_r_scenario_sostanza.id_sostanza
+             and siig_r_area_danno.flg_lieve = siig_r_scenario_sostanza.flg_lieve
+        )
+        ) * (1 - coalesce((
+        select cff
         from siig_r_arco_1_scen_tipobers
         where id_geo_arco = siig_geo_ln_arco_1.id_geo_arco
-        and id_scenario = 1--siig_r_scenario_sostanza.id_scenario
-        and id_bersaglio = 3--12
-        )) * (
+        and id_scenario = siig_r_scenario_sostanza.id_scenario
+        and id_bersaglio = 11
+        ),0.3)) * (
                 select suscettibilita
                 from siig_r_scenario_gravita
                 inner join siig_d_gravita on siig_r_scenario_gravita.id_gravita = siig_d_gravita.id_gravita
-                where id_scenario = 1--siig_r_scenario_sostanza.id_scenario
-                and fk_bersaglio = 13--12
-        ) + 
+                where id_scenario = siig_r_scenario_sostanza.id_scenario
+                and fk_bersaglio = 11
+        ) * 1
+     ) + (
         (select coalesce(mq_zone_urbanizzate,1)
         from siig_t_vulnerabilita_1
-        where id_geo_arco = siig_geo_ln_arco_1.id_geo_arco) * (1 - (
-        select cff 
+        where id_geo_arco = siig_geo_ln_arco_1.id_geo_arco
+        and id_distanza = (
+             select fk_distanza
+             from siig_r_area_danno
+             inner join siig_d_gravita on siig_r_area_danno.id_gravita = siig_d_gravita.id_gravita
+             where siig_d_gravita.fk_bersaglio = 10             
+             and siig_r_area_danno.id_scenario = siig_r_scenario_sostanza.id_scenario
+             and siig_r_area_danno.id_sostanza = siig_r_scenario_sostanza.id_sostanza
+             and siig_r_area_danno.flg_lieve = siig_r_scenario_sostanza.flg_lieve
+        )
+        ) * (1 - coalesce((
+        select cff
         from siig_r_arco_1_scen_tipobers
         where id_geo_arco = siig_geo_ln_arco_1.id_geo_arco
-        and id_scenario = 1--siig_r_scenario_sostanza.id_scenario
-        and id_bersaglio = 3--10
-        )) * (
+        and id_scenario = siig_r_scenario_sostanza.id_scenario
+        and id_bersaglio = 10
+        ),0.3)) * (
                 select suscettibilita
                 from siig_r_scenario_gravita
                 inner join siig_d_gravita on siig_r_scenario_gravita.id_gravita = siig_d_gravita.id_gravita
-                where id_scenario = 1--siig_r_scenario_sostanza.id_scenario
-                and fk_bersaglio = 13--10
-        ) + 
+                where id_scenario = siig_r_scenario_sostanza.id_scenario
+                and fk_bersaglio = 10
+        ) * 1
+     ) + (
         (select coalesce(mq_beni_culturali,1)
         from siig_t_vulnerabilita_1
-        where id_geo_arco = siig_geo_ln_arco_1.id_geo_arco) * (1 - (
-        select cff 
+        where id_geo_arco = siig_geo_ln_arco_1.id_geo_arco
+        and id_distanza = (
+             select fk_distanza
+             from siig_r_area_danno
+             inner join siig_d_gravita on siig_r_area_danno.id_gravita = siig_d_gravita.id_gravita
+             where siig_d_gravita.fk_bersaglio = 16             
+             and siig_r_area_danno.id_scenario = siig_r_scenario_sostanza.id_scenario
+             and siig_r_area_danno.id_sostanza = siig_r_scenario_sostanza.id_sostanza
+             and siig_r_area_danno.flg_lieve = siig_r_scenario_sostanza.flg_lieve
+        )) * (1 - coalesce((
+        select cff
         from siig_r_arco_1_scen_tipobers
         where id_geo_arco = siig_geo_ln_arco_1.id_geo_arco
-        and id_scenario = 1--siig_r_scenario_sostanza.id_scenario
-        and id_bersaglio = 3--16
-        )) * (
+        and id_scenario = siig_r_scenario_sostanza.id_scenario
+        and id_bersaglio = 16
+        ),0.3)) * (
                 select suscettibilita
                 from siig_r_scenario_gravita
                 inner join siig_d_gravita on siig_r_scenario_gravita.id_gravita = siig_d_gravita.id_gravita
-                where id_scenario = 1--siig_r_scenario_sostanza.id_scenario
-                and fk_bersaglio = 13--16
-        ) + 
+                where id_scenario = siig_r_scenario_sostanza.id_scenario
+                and fk_bersaglio = 16
+        ) * 1
+     ) + (
         (select coalesce(mq_acque_superficiali,1)
         from siig_t_vulnerabilita_1
-        where id_geo_arco = siig_geo_ln_arco_1.id_geo_arco) * (1 - (
-        select cff 
+        where id_geo_arco = siig_geo_ln_arco_1.id_geo_arco
+        and id_distanza = (
+             select fk_distanza
+             from siig_r_area_danno
+             inner join siig_d_gravita on siig_r_area_danno.id_gravita = siig_d_gravita.id_gravita
+             where siig_d_gravita.fk_bersaglio = 15             
+             and siig_r_area_danno.id_scenario = siig_r_scenario_sostanza.id_scenario
+             and siig_r_area_danno.id_sostanza = siig_r_scenario_sostanza.id_sostanza
+             and siig_r_area_danno.flg_lieve = siig_r_scenario_sostanza.flg_lieve
+        )
+        ) * (1 - coalesce((
+        select cff
         from siig_r_arco_1_scen_tipobers
         where id_geo_arco = siig_geo_ln_arco_1.id_geo_arco
-        and id_scenario = 1--siig_r_scenario_sostanza.id_scenario
-        and id_bersaglio = 3--15
-        )) * (
+        and id_scenario = siig_r_scenario_sostanza.id_scenario
+        and id_bersaglio = 15
+        ),0.3)) * (
                 select suscettibilita
                 from siig_r_scenario_gravita
                 inner join siig_d_gravita on siig_r_scenario_gravita.id_gravita = siig_d_gravita.id_gravita
-                where id_scenario = 1--siig_r_scenario_sostanza.id_scenario
-                and fk_bersaglio = 13--15
-        ) + 
+                where id_scenario = siig_r_scenario_sostanza.id_scenario
+                and fk_bersaglio = 15
+        ) * 1
+     ) + (
         (select coalesce(mq_acque_sotterranee,1)
         from siig_t_vulnerabilita_1
-        where id_geo_arco = siig_geo_ln_arco_1.id_geo_arco) * (1 - (
-        select cff 
+        where id_geo_arco = siig_geo_ln_arco_1.id_geo_arco
+        and id_distanza = (
+             select fk_distanza
+             from siig_r_area_danno
+             inner join siig_d_gravita on siig_r_area_danno.id_gravita = siig_d_gravita.id_gravita
+             where siig_d_gravita.fk_bersaglio = 14             
+             and siig_r_area_danno.id_scenario = siig_r_scenario_sostanza.id_scenario
+             and siig_r_area_danno.id_sostanza = siig_r_scenario_sostanza.id_sostanza
+             and siig_r_area_danno.flg_lieve = siig_r_scenario_sostanza.flg_lieve
+        )) * (1 - coalesce((
+        select cff
         from siig_r_arco_1_scen_tipobers
         where id_geo_arco = siig_geo_ln_arco_1.id_geo_arco
-        and id_scenario = 1--siig_r_scenario_sostanza.id_scenario
-        and id_bersaglio = 3--14
-        )) * (
+        and id_scenario = siig_r_scenario_sostanza.id_scenario
+        and id_bersaglio = 14
+        ),0.3)) * (
                 select suscettibilita
                 from siig_r_scenario_gravita
                 inner join siig_d_gravita on siig_r_scenario_gravita.id_gravita = siig_d_gravita.id_gravita
-                where id_scenario = 1--siig_r_scenario_sostanza.id_scenario
-                and fk_bersaglio = 13--14
-        )   
-     )) as psc
+                where id_scenario = siig_r_scenario_sostanza.id_scenario
+                and fk_bersaglio = 14
+        ) * 1
+     ))
+     
+     ) as psc
      from siig_r_scenario_sostanza
      where siig_r_scenario_sostanza.id_sostanza = siig_r_arco_1_sostanza.id_sostanza
-     and siig_r_scenario_sostanza.id_scenario in (1)--param scenario single value or comma delimited list   
-     and flg_lieve in (0,1)--param gravita 0 1 o 0,1
+     and siig_r_scenario_sostanza.id_scenario in (1,2,3,4,5,6,7,8,9,10,11)
+     and flg_lieve in (0,1)
      )) as rischio
      
      from siig_r_arco_1_sostanza
      where siig_r_arco_1_sostanza.id_geo_arco = siig_geo_ln_arco_1.id_geo_arco
-     and id_sostanza in (1,9) --param sostanza single value or comma delimited list    
-) as rischio,siig_geo_ln_arco_1.geometria
+     and id_sostanza in (1,2,3,4,5,6,7,8,9,10)
+) as rischio,siig_geo_ln_arco_1.lunghezza,siig_geo_ln_arco_1.geometria
 from siig_geo_ln_arco_1
+where siig_geo_ln_arco_1.geometria && st_makeenvelope(437283.798308,4973519.681172,440915.445091,4975761.297918, 32632)
 order by id_geo_arco
--- database/rischio_totale_umani.sql
+++ database/rischio_totale_umani.sql
@@ -1,53 +1,140 @@
 select siig_geo_ln_arco_1.id_geo_arco,siig_geo_ln_arco_1.nr_incidenti_elab * (
      select sum(siig_r_arco_1_sostanza.padr * 
      (
-     select sum(siig_r_scenario_sostanza.psc * (
-        ((select nr_pers_residenti
+     select sum(siig_r_scenario_sostanza.psc * ((
+        ((select coalesce(nr_pers_residenti,1)
         from siig_t_vulnerabilita_1
         where id_geo_arco = siig_geo_ln_arco_1.id_geo_arco
         and id_distanza = (
              select fk_distanza
              from siig_r_area_danno
              inner join siig_d_gravita on siig_r_area_danno.id_gravita = siig_d_gravita.id_gravita
-             where siig_d_gravita.fk_bersaglio = 1
-             and siig_d_gravita.id_gravita = 1
-             and siig_r_area_danno.id_scenario = 1--siig_r_scenario_sostanza.id_scenario
+             where siig_d_gravita.id_gravita = 1             
+             and siig_r_area_danno.id_scenario = siig_r_scenario_sostanza.id_scenario
              and siig_r_area_danno.id_sostanza = siig_r_scenario_sostanza.id_sostanza
              and siig_r_area_danno.flg_lieve = siig_r_scenario_sostanza.flg_lieve
-        ))  * (
+        )
+        ) * (
                 select suscettibilita
                 from siig_r_scenario_gravita
                 inner join siig_d_gravita on siig_r_scenario_gravita.id_gravita = siig_d_gravita.id_gravita
-                where id_scenario = 1--siig_r_scenario_sostanza.id_scenario
-                and fk_bersaglio = 13
+                where id_scenario = siig_r_scenario_sostanza.id_scenario
                 and siig_d_gravita.id_gravita = 1
-        ) + 
-        () * (
+        ) + ((select coalesce(nr_pers_residenti,1)
+        from siig_t_vulnerabilita_1
+        where id_geo_arco = siig_geo_ln_arco_1.id_geo_arco
+        and id_distanza = (
+             select fk_distanza
+             from siig_r_area_danno
+             inner join siig_d_gravita on siig_r_area_danno.id_gravita = siig_d_gravita.id_gravita
+             where siig_d_gravita.id_gravita = 35             
+             and siig_r_area_danno.id_scenario = siig_r_scenario_sostanza.id_scenario
+             and siig_r_area_danno.id_sostanza = siig_r_scenario_sostanza.id_sostanza
+             and siig_r_area_danno.flg_lieve = siig_r_scenario_sostanza.flg_lieve
+        )) - (select coalesce(nr_pers_residenti,1)
+        from siig_t_vulnerabilita_1
+        where id_geo_arco = siig_geo_ln_arco_1.id_geo_arco
+        and id_distanza = (
+             select fk_distanza
+             from siig_r_area_danno
+             inner join siig_d_gravita on siig_r_area_danno.id_gravita = siig_d_gravita.id_gravita
+             where siig_d_gravita.id_gravita = 1             
+             and siig_r_area_danno.id_scenario = siig_r_scenario_sostanza.id_scenario
+             and siig_r_area_danno.id_sostanza = siig_r_scenario_sostanza.id_sostanza
+             and siig_r_area_danno.flg_lieve = siig_r_scenario_sostanza.flg_lieve
+        ))
+        ) * (
                 select suscettibilita
                 from siig_r_scenario_gravita
                 inner join siig_d_gravita on siig_r_scenario_gravita.id_gravita = siig_d_gravita.id_gravita
-                where id_scenario = 1--siig_r_scenario_sostanza.id_scenario
-                and fk_bersaglio = 13
-                and siig_d_gravita.id_gravita = 1
-        ) )
-        * (1 - (
-        select cff 
+                where id_scenario = siig_r_scenario_sostanza.id_scenario
+                and siig_d_gravita.id_gravita = 35
+        ) +  (
+        ((select coalesce(nr_pers_residenti,1)
+        from siig_t_vulnerabilita_1
+        where id_geo_arco = siig_geo_ln_arco_1.id_geo_arco
+        and id_distanza = (
+             select fk_distanza
+             from siig_r_area_danno
+             inner join siig_d_gravita on siig_r_area_danno.id_gravita = siig_d_gravita.id_gravita
+             where siig_d_gravita.id_gravita = 2             
+             and siig_r_area_danno.id_scenario = siig_r_scenario_sostanza.id_scenario
+             and siig_r_area_danno.id_sostanza = siig_r_scenario_sostanza.id_sostanza
+             and siig_r_area_danno.flg_lieve = siig_r_scenario_sostanza.flg_lieve
+        )) - ((select coalesce(nr_pers_residenti,1)
+        from siig_t_vulnerabilita_1
+        where id_geo_arco = siig_geo_ln_arco_1.id_geo_arco
+        and id_distanza = (
+             select fk_distanza
+             from siig_r_area_danno
+             inner join siig_d_gravita on siig_r_area_danno.id_gravita = siig_d_gravita.id_gravita
+             where siig_d_gravita.id_gravita = 35             
+             and siig_r_area_danno.id_scenario = siig_r_scenario_sostanza.id_scenario
+             and siig_r_area_danno.id_sostanza = siig_r_scenario_sostanza.id_sostanza
+             and siig_r_area_danno.flg_lieve = siig_r_scenario_sostanza.flg_lieve
+        ))
+        ) * (
+                select suscettibilita
+                from siig_r_scenario_gravita
+                inner join siig_d_gravita on siig_r_scenario_gravita.id_gravita = siig_d_gravita.id_gravita
+                where id_scenario = siig_r_scenario_sostanza.id_scenario
+                and siig_d_gravita.id_gravita = 2
+        ) + ((select coalesce(nr_pers_residenti,1)
+        from siig_t_vulnerabilita_1
+        where id_geo_arco = siig_geo_ln_arco_1.id_geo_arco
+        and id_distanza = (
+             select fk_distanza
+             from siig_r_area_danno
+             inner join siig_d_gravita on siig_r_area_danno.id_gravita = siig_d_gravita.id_gravita
+             where siig_d_gravita.id_gravita = 3             
+             and siig_r_area_danno.id_scenario = siig_r_scenario_sostanza.id_scenario
+             and siig_r_area_danno.id_sostanza = siig_r_scenario_sostanza.id_sostanza
+             and siig_r_area_danno.flg_lieve = siig_r_scenario_sostanza.flg_lieve
+        )) - (select coalesce(nr_pers_residenti,1)
+        from siig_t_vulnerabilita_1
+        where id_geo_arco = siig_geo_ln_arco_1.id_geo_arco
+        and id_distanza = (
+             select fk_distanza
+             from siig_r_area_danno
+             inner join siig_d_gravita on siig_r_area_danno.id_gravita = siig_d_gravita.id_gravita
+             where siig_d_gravita.id_gravita = 2             
+             and siig_r_area_danno.id_scenario = siig_r_scenario_sostanza.id_scenario
+             and siig_r_area_danno.id_sostanza = siig_r_scenario_sostanza.id_sostanza
+             and siig_r_area_danno.flg_lieve = siig_r_scenario_sostanza.flg_lieve
+        ))) 
+        * (
+                select suscettibilita
+                from siig_r_scenario_gravita
+                inner join siig_d_gravita on siig_r_scenario_gravita.id_gravita = siig_d_gravita.id_gravita
+                where id_scenario = siig_r_scenario_sostanza.id_scenario
+                and siig_d_gravita.id_gravita = 3
+        )
+        ))
+        
+        ) * (1 - coalesce((
+        select cff
         from siig_r_arco_1_scen_tipobers
         where id_geo_arco = siig_geo_ln_arco_1.id_geo_arco
-        and id_scenario = 1--siig_r_scenario_sostanza.id_scenario
-        and id_bersaglio = 3--13
-        ))
-           
-     )) as psc
+        and id_scenario = siig_r_scenario_sostanza.id_scenario
+        and id_bersaglio = 1
+        ),0.3)) * (
+                select  fp_scen_centrale 
+                from siig_t_bersaglio
+                where id_bersaglio = 1
+        ) * 1
+     ))
+     
+     ) as psc
      from siig_r_scenario_sostanza
      where siig_r_scenario_sostanza.id_sostanza = siig_r_arco_1_sostanza.id_sostanza
-     and siig_r_scenario_sostanza.id_scenario in (1)--param scenario single value or comma delimited list   
-     and flg_lieve in (0,1)--param gravita 0 1 o 0,1
+     and siig_r_scenario_sostanza.id_scenario in (1,2,3,4,5,6,7,8,9,10,11)
+     and flg_lieve in (0,1)
      )) as rischio
      
      from siig_r_arco_1_sostanza
      where siig_r_arco_1_sostanza.id_geo_arco = siig_geo_ln_arco_1.id_geo_arco
-     and id_sostanza in (9) --param sostanza single value or comma delimited list    
-) as rischio,siig_geo_ln_arco_1.geometria
+     and id_sostanza in (1,2,3,4,5,6,7,8,9,10)
+) as rischio,siig_geo_ln_arco_1.lunghezza,siig_geo_ln_arco_1.geometria
 from siig_geo_ln_arco_1
+where siig_geo_ln_arco_1.geometria && st_makeenvelope(437283.798308,4973519.681172,440915.445091,4975761.297918, 32632)
 order by id_geo_arco
