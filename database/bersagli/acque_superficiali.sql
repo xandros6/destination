78cfdcd3de6de165c8b1d1f1e5e275b7ffd0c561
Damiano <damianofds@gmail.com>
2013-06-12 18:40:36 +0200

    #4 moved bersagli sql to database directory

---
+++ database/bersagli/acque_superficiali.sql
@@ -0,0 +1,13 @@
+insert into siig_geo_bers_non_umano_pl(idgeo_bers_non_umano_pl,geometria)
+select 1924000+fid,the_geom
+from "RP_V_BNU_ASUP_C_02";
+
+insert into siig_t_bersaglio_non_umano(id_tematico,id_bersaglio,id_partner,fk_bers_non_umano_pl,denominazione,fk_classe_clc,toponimo_completo,superficie)
+select "ID_TEMA",15,1,1924000+fid,"DENOM","FK_CLC","TOPONIMO","SUPERFICIE"
+from "RP_V_BNU_ASUP_C_02";
+
+insert into siig_geo_bers_non_umano_ln(idgeo_bers_non_umano_ln,geometria)
+select fid,the_geom
+from "RP_V_BNU_ASUP_C_03";
+
+update siig_t_bersaglio_non_umano set fk_bers_non_umano_ln=(select fid from "RP_V_BNU_ASUP_C_03" c where "ID_TEMA"=id_tematico) where id_bersaglio=15;
